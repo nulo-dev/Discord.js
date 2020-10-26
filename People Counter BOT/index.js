21b98afc5cd55b02f9547b579d166f4b57cf0143
nulo-dev <72146375+nulo-dev@users.noreply.github.com>
2020-10-26 19:38:37 +0100

    Update index.js

--- People Counter BOT/index.js
+++ People Counter BOT/index.js
@@ -1,33 +1,34 @@
 const discord = require('discord.js');
 const client = new discord.Client();
 
-const token = require('./config.json').token;
+const token = "YOUR TOKEN HERE";
 
 client.login(token);
 
+//counts users when bot starts//
 client.on('ready', () => {
     let myGuild = client.guilds.get('638110806621290496');
     let memberCount = myGuild.memberCount;
     let memberCountChannel = myGuild.channels.get('705868078784970823');
-    memberCountChannel.setName('¡SOMOS ' + memberCount + ' USUARIOS!')
+    memberCountChannel.setName('¡WE ARE ' + memberCount + ' USERS!')
     .then(result => console.log(result))
     .catch(error => console.log(error));
 });
-
+//counts users when a member joins the discord//
 client.on('guildMemberAdd', member => {
     let myGuild = client.guilds.get('638110806621290496');
     let memberCount = myGuild.memberCount;
     let memberCountChannel = myGuild.channels.get('705868078784970823');
-    memberCountChannel.setName('¡SOMOS ' + memberCount + ' USUARIOS!')
+    memberCountChannel.setName('¡WE ARE ' + memberCount + ' USERS!')
     .then(result => console.log(result))
     .catch(error => console.log(error));
 });
-
+//counts users when a member left the discord//
 client.on('guildMemberRemove', member => {
     let myGuild = client.guilds.get('638110806621290496');
     let memberCount = myGuild.memberCount;
     let memberCountChannel = myGuild.channels.get('705868078784970823');
-    memberCountChannel.setName('¡SOMOS ' + memberCount + ' USUARIOS!')
+    memberCountChannel.setName('¡WE ARE ' + memberCount + ' USERS!')
     .then(result => console.log(result))
     .catch(error => console.log(error));
 });
@@ -37,7 +38,7 @@ client.on('message', (message, user) => {
 if (message.content === "!usuarios") {
     let myGuild = client.guilds.get('638110806621290496');
     let memberCount = myGuild.memberCount;
-    message.channel.send("Este server tiene "+memberCount+" miembros en total") //Mensaje en donde el bot muestra los usuarios del server
+    message.channel.send("This server has "+memberCount+" members in total") //Message where the bot shows the users of the server//
 }
 
 });
