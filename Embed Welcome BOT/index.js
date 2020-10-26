fa82a4fb9e1e54d0eb9f97d4ad1bbe855e2c2367
nulo-dev <72146375+nulo-dev@users.noreply.github.com>
2020-10-26 19:53:38 +0100

    Update index.js

--- Embed Welcome BOT/index.js
+++ Embed Welcome BOT/index.js
@@ -7,60 +7,53 @@ const { get } = require('snekfetch');
 const fs = require('fs');
 const superagent = require('superagent');
 
-
-
 const PREFIX = '!';
 
 var version = '1.0.1';
 
 bot.on('ready', () =>{
-    console.log('El bot esta online');
+    console.log('nulo-dev bot is active!');
 })
 
 
 bot.on('guildMemberAdd', async member =>{
 
-let namam = member.user.username
-let batasnama = namam.length > 12 ? namam.substring(0.10) + "..." : namam;
+let name = member.user.username
+let namelimit = name.length > 12 ? name.substring(0.10) + "..." : name;
     async function createCanvas() {
         Canvas.registerFont('DoctorGlitch.otf', {family: 'DoctorGlitch'});
         let imageUrlPhoto = /\?size=1024$/g;
-        let myGuild = bot.guilds.get('638110806621290496');
+        let myGuild = bot.guilds.get('YOUR GUILD ID'); //Right click on the server and "copy id"//
         let memberCount = myGuild.memberCount;
-        let image = 'https://i.imgur.com/k3lPUMx.jpg';
+        let image = 'BACKGROUND URL'; //PUT A URL THAT ENDS WITH THE PREFIX OF THE PHOTO, EXAMPLE: https://www.google.com/logos/google.jpg //
         let {body: background} = await superagent.get(image);
         let {body: avatar} = await superagent.get(member.user.displayAvatarURL.replace(imageUrlPhoto, "?size=100"));
         let borde = "https://nmwrri.nmsu.edu/IBWC/2019/photos/plain-white-background.jpg"
         return new Canvas(856, 376)
             .setColor('#ffffff')
-            .setTextFont('bold 50px DoctorGlitch')
+            .setTextFont('bold 50px DoctorGlitch') //You can change the font//
             .lineTo(50, 102)
             .addImage(background, 0, 0, 856, 376)
             .setTextAlign('center')
-            .addText(`Bienvenido`, 428,280)
-            .setTextFont('bold 30px DoctorGlitch')
+            .addText(`Welcome`, 428,280)
+            .setTextFont('bold 30px DoctorGlitch')//You can change the font//
             .setTextAlign('center')
             .setColor('#29f0d8')
-            .addText(`${batasnama}`, 428,320)
-            .setTextFont('bold 20px DoctorGlitch')
+            .addText(`${namelimit}`, 428,320)
+            .setTextFont('bold 20px DoctorGlitch')//You can change the font//
             .setColor('#ffffff')
-            .addText(`¡Contigo somos ${memberCount} miembros!`, 428,350)
+            .addText(`¡With you we are ${memberCount} members!`, 428,350)
             .setColor('#29f0d8')
             .addCircle(432, 120, 105)
             .addRoundImage(avatar, 332, 20, 200, 200, 100)
             .toBufferAsync();
     }
 
-    let channel = member.guild.channels.get('769204134968426496')
-const Mensaje = {
-    color: 1752220,
-    title: `<a:pepeShoot:659762885706383380> | **Hola ${member.user.username}!**`,
-    description: `**Bienvenid@ a:** \n Osaka RP ¡Yōkoso, asonde tanoshinde kudasai!`,
-};
+    let channel = member.guild.channels.get('YOUR WELCOME CHANNEL ID') //right click on the channel you want to see the welcome and "copy id"//
 
 const file = {
     attachment: await createCanvas(),
-    name:'bienvenida.jpg'
+    name:'welcome.jpg' //this names the image name, you can change it :D //
 }
     
         channel.send({files : [file]});
