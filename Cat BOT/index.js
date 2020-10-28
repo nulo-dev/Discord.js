c0e32e50346f619b6a62103e9a1cd97c3066cded
nulo-dev <72146375+nulo-dev@users.noreply.github.com>
2020-10-28 17:56:03 +0100

    Create index.js

---
+++ Cat BOT/index.js
@@ -0,0 +1,21 @@
+
+const {Client, RichEmbed} = require('discord.js');
+const Discord = require('discord.js')
+const bot = new Client();
+const superagent = require('superagent')
+const { get } = require("snekfetch"); 
+
+bot.on("message", async (message,args) => {
+                if(message.content.startsWith(PREFIX + 'gato')) {
+                    try {
+                        get('https://aws.random.cat/meow').then(res => {
+                            const embed = new Discord.RichEmbed()
+                            .setImage(res.body.file)
+                            return message.channel.send({embed});
+                        });
+                    } catch(err) {
+                        console.log(err.stack)
+                       // return message.channel.send(err.stack);
+                    }
+                }
+          });
