16f7f0454373820c06bc1fc08e5960aa995e4b27
nulo-dev <72146375+nulo-dev@users.noreply.github.com>
2020-11-01 15:38:22 +0100

    Create index.js

---
+++ User-Info Bot/index.js
@@ -0,0 +1,28 @@
+const token = 'YOUR TOKEN';
+
+const PREFIX = '!';
+
+if (message.content.startsWith('!user-info')) {
+    const user = message.mentions.users.first() || message.author;
+    let member = message.mentions.members.first() || message.member;
+    const moment= require('moment') 
+    
+    let status = {
+        "online": "Connected",
+        "offline": "Disconnected",
+        "idle": "Busy",
+        "dnd": "Do not disturb",
+
+    }
+    const infoEmbed = new RichEmbed()
+        .setColor(0x333333)
+        .setAuthor(user.username)
+        .setImage(user.displayAvatarURL)
+        .addField("Full name", `${user.tag}`)
+        .addField("ID", `${user.id}`)
+        .addField("Status", `${status[member.presence.status]}`)
+        .addField("Nickname", `${member.nickname}`)
+        .addField("Joined the server", `${moment.utc(member.joinedAt).format('DD/MM/YY HH:mm')}`)
+        .addField('Rols', member.roles.map(r => `${r}`).join(' | '))
+    message.channel.send(infoEmbed);
+}
